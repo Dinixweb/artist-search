26bfe8343baeb723ac643fb1c7a8f402f8482360
dinixweb <sd4live@gmail.com>
2022-07-20 19:10:05 +0100

    json server updates

--- controllers/artist.js
+++ controllers/artist.js
@@ -21,40 +21,70 @@ exports.findUserByName = async (req, res) => {
     let getArtistData = await axios.get(
       `${API_BASE_URL}?method=artist.search&api_key=${API_KEY}&format=json&artist=${artistName}`
     );
-
     // this will return only result without search information
     let artistBio = getArtistData.data.results.artistmatches.artist;
 
-    // csv table column header
-    const tableColumn = ["name", "mbid", "url", "image"];
+    // get all artist from json db
+    let getArtistJsonDB = [];
+    const randomArtist = await axios.get("http://localhost:5000/artist");
+    getArtistJsonDB.push(randomArtist.data);
 
-    // this will filter only data matching column header
-    artistBio = artistBio.map((list, i) => {
-      return Object.keys(list)
-        .filter((key) => tableColumn.includes(key))
-        .reduce((obj, key) => {
-          return Object.assign(obj, {
-            [key]: list[key],
+    if (artistBio.length <= 0) {
+      res.status(201).send(randomArtist.data);
+      return;
+    } else {
+      // csv table column header
+      const tableColumn = ["name", "mbid", "url", "image"];
+
+      // this will filter only data matching column header
+      artistBio = artistBio.map((list, i) => {
+        return Object.keys(list)
+          .filter((key) => tableColumn.includes(key))
+          .reduce((obj, key) => {
+            return Object.assign(obj, {
+              [key]: list[key],
+            });
+          }, {});
+      });
+
+      //return all image
+      let image = artistBio.map((obj) => obj.image);
+
+      //this will return all small images belonging to each artist
+      let small_image = artistBio.map((obj) => [obj.image[0]["#text"]]).flat();
+
+      // this will re-apend the images to artist data in the order of the data
+      artistBio.forEach((e, i) => {
+        return (e.image = image[i]), (e.small_image = small_image[i]);
+      });
+
+      // destructure the array to one level
+      [getArtistJsonDB] = getArtistJsonDB.map((obj) => obj.flat());
+
+      // return all artist name from the json db for check purposes
+      getArtistJsonDB = getArtistJsonDB.map((artistExist) => {
+        return artistExist.name;
+      });
+
+      // inorder to add an artist multiple times
+      // we do a check to know the artist already exist in the json db
+      // else will add the artist
+      const artistObj = [];
+      artistBio.map((data) => {
+        if (getArtistJsonDB.includes(data.name)) {
+        } else {
+          axios.post(`http://localhost:5000/artist`, data).then((res) => {
+            artistObj.push(res.data);
           });
-        }, {});
-    });
+        }
+      });
 
-    //return all image
-    let image = artistBio.map((obj) => obj.image);
+      // create csv
+      writeCSVFile(artistBio, artistName);
 
-    //this will return all small images belonging to each artist
-    let small_image = artistBio.map((obj) => [obj.image[0]["#text"]]).flat();
-
-    // this will re-apend the images to artist data in the order of the data
-    artistBio.forEach((e, i) => {
-      return (e.image = image[i]), (e.small_image = small_image[i]);
-    });
-
-    // create csv
-    writeCSVFile(artistBio, artistName);
-
-    // response
-    res.status(200).send(artistBio);
+      // response
+      res.status(200).send(artistBio);
+    }
   } catch (error) {
     console.log(error);
     res.status(404).send(new Api404Error());
